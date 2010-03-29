ca98347000b958b7cf8768cfaeb28f93814c8b63
Sergey Pariev <spariev@gmail.com>
2010-03-29 10:48:58 +0400

    :doc and :result metadata are unnecessary now, pprint examples and eval them to get results

--- examples.clj
+++ examples.clj
@@ -2,103 +2,102 @@
 ;; licensed under the Creative Commons Attribution/Share-Alike License - http://creativecommons.org/licenses/by-sa/3.0/ 
 
 [
-#^{:example "loop" :result 120 :doc "computes the factorial of 5 with loop/recur"}
+#^{:example "loop" :doc "computes the factorial of 5 with loop/recur"}
 (loop [cnt 5 acc 1]
   (if (zero? cnt)
     acc
     (recur (dec cnt) (* acc cnt))))
 
-#^{:example "reduce, range" :result 2500 :doc "(range 1 100 2) creates a lazy sequence of numbers 1 3 5 7 ... 99. 1 is the starting point, 100 is the end point, 2 is the step. reduce calls the + function."}
+#^{:example "reduce, range" :doc "(range 1 100 2) creates a lazy sequence of numbers 1 3 5 7 ... 99. 1 is the starting point, 100 is the end point, 2 is the step. reduce calls the + function."}
 (reduce + (range 1 100 2))
 
-#^{:example "nth,random" :result "hello" :doc "nth selects a value from the vector of words we supplied, in this case we called rand to get a number between 0 (inclusive) and 5 (exclusive)."}
+#^{:example "nth,random" :doc "nth selects a value from the vector of words we supplied, in this case we called rand to get a number between 0 (inclusive) and 5 (exclusive)."}
 (nth ["hello" "bye" "foo" "bar" "baz"] (rand 5))
 
 
-#^{:example "get" :result 1 :doc ""}
+#^{:example "get"}
 (get {:a 1 :b 2} :a)
 
-#^{:example "get" :result 0 :doc "get also accepts an optional third argument, which is returned if key is not found in map"}
+#^{:example "get" :doc "get also accepts an optional third argument, which is returned if key is not found in map"}
 (get {:a 1 :b 2} :e 0)
 
-#^{:example "assoc-in" :result {:level 0, :nested1 {:level 1, :final-data "new data"}} :doc ""}
+#^{:example "assoc-in"}
 (let [nested-structure { :level 0 :nested1
 			{ :level 1 :final-data "initial data"}}]
  (assoc-in nested-structure [:nested1 :final-data] "new data")) 
 
-#^{:example "merge" :result {:a 1, :b 2} :doc "Combine two maps into a more massive map"}
+#^{:example "merge" :doc "Combine two maps into a more massive map"}
 (merge {:a 1} {:b 2})
 
-#^{:example "merge-with" :result {:a 3 :b 3} :doc ""}
+#^{:example "merge-with"}
 (merge-with + {:a 1} {:a 2, :b 3})
 
-#^{:example "union" :result #{1 2 3 4 7} :doc ""}
+#^{:example "union"}
 (clojure.set/union #{1 2 3} #{1 4 7})
 
-#^{:example "zipmap" :result {4 5, 3 4, 1 2}  :doc ""}
+#^{:example "zipmap"}
 (let [ks [1 3 4]] (zipmap ks (map inc ks)))
 
 
-#^{:example "filter" :result (nil nil)  :doc ""}
+#^{:example "filter"}
 (filter nil? [:a :b nil nil :a]) 
 
-#^{:example "filter" :result (:b) :doc ""}
+#^{:example "filter"}
 (filter (fn[x](= x :b)) [:a :b nil nil :a])
 
-#^{:example "remove" :result (:a :b :a) :doc ""}
+#^{:example "remove"}
 (remove nil? [:a :b nil nil :a]) 
 
-#^{:example "some" :result :a :doc ""}
+#^{:example "some"}
 (some #{:a} [:a :b])
 
-#^{:example "every" :result false :doc ""}
+#^{:example "every"}
 (every? #{:a :b} [:a :b :c])
 
-#^{:example "every" :result true :doc ""}
+#^{:example "every"}
 (every? #{:a :b} [:a :b])
 
 
-#^{:example "interpose" :result "hi|mum|and|dad" :doc ""}
+#^{:example "interpose"}
 (apply str (interpose "|" ["hi" "mum" "and" "dad"]))
 
-#^{:example "interleave" :result (1 :a 2 :b 3 :c) :doc ""}
+#^{:example "interleave" :result (1 :a 2 :b 3 :c)}
 (interleave [1 2 3] [:a :b :c])
 
-#^{:example "apply,interpose,reverse" :result "cold am I" :doc ""}
+#^{:example "apply,interpose,reverse"}
 (apply str (interpose " " (reverse (.split "I am cold" " "))))
 
-#^{:example "butlast" :result (\h \e \l \l) :doc ""}
+#^{:example "butlast"}
 (butlast "hello")
 
-#^{:example "replace" :result "heo word"  :doc ""}
+#^{:example "replace"}
 (apply str (replace {\l ""} "hello world"))
 
-#^{:example "conj" :result [1 2 3 4] :doc ""}
+#^{:example "conj"}
 (conj [1 2 3] 4)
 
-#^{:example "conj" :result (\d :a :b :c) :doc ""}
+#^{:example "conj"}
 (conj '(:a :b :c) \d)
 
-#^{:example "map" :result (2 4 6 8)  :doc ""}
+#^{:example "map"}
 (map + [1 2 3 4] [1 2 3 4])
 
-#^{:example "reduce,filter,range" :result 2500 :doc "sum the odd numbers to 100"}
+#^{:example "reduce,filter,range" :doc "sum the odd numbers to 100"}
 (reduce + (filter odd? (range 100)))
 
-#^{:example "->" :result "A-C"  :doc ""}
+#^{:example "->"}
 (-> "abc" .toUpperCase (.replace "B" "-"))
 
-#^{:example "->" :result (2 3 4) :doc ""}
+#^{:example "->"}
 (-> true (if inc dec) (map [1 2 3]))
 
-#^{:example "cond" :result 100 :doc ""}
+#^{:example "cond"}
 (#(cond (= 1 %) 1 (> % 1) 100 :else 0) 5)
 
-#^{:example "condp" :result "fun" :doc ""}
+#^{:example "condp"}
 (let [a 5]
   (condp = a
     1 "hi"
     5 "fun"))
-
 
 ]
--- src/spariev/find_example.clj
+++ src/spariev/find_example.clj
@@ -1,5 +1,6 @@
 (ns spariev.find-example
-  (:require clojure.contrib.http.agent))
+  (:use [clojure.contrib.http.agent :as http :only [string http-agent]]
+	[clojure.contrib.pprint :as pp]))
 
 (def *all-examples* (ref []))
 
@@ -7,24 +8,26 @@
      "http://github.com/spariev/find-example/raw/master/examples.clj")
 
 (defn- load-examples
-  [refetch]  
+  [refetch]
   (when (or (= 0 (count @*all-examples*)) refetch)
     (let [new-examples
 	  (read (java.io.PushbackReader.
 		 (java.io.StringReader.
-		  (clojure.contrib.http.agent/string (clojure.contrib.http.agent/http-agent *examples-url*)))))]
+		  (http/string (http/http-agent *examples-url*)))))]
       (dosync
        (ref-set *all-examples* new-examples))))
   @*all-examples*)
-      
-	  
+
+
 (defn- print-example
   [v]
   (println "-------------------------")
   (println "Example for" (:example (meta v)))
   (println (:doc (meta v)))
-  (prn v)
-  (println "=>" (:result (meta v))))
+  (binding [*print-right-margin* 40]
+    (with-pprint-dispatch *code-dispatch* (pprint v)))
+  (print "=> ")
+  (pprint (eval v)))
 
 (defn find-example
   "Finds and prints example which :example or :doc metadata contains a match for re-string-or-pattern. If refetch is true, refetches examples file from github."
@@ -34,7 +37,6 @@
     (let [re (re-pattern re-string-or-pattern)]
       (doseq [example (load-examples refetch)
               :when (and (:example (meta example))
-			 (:doc (meta example))
-                         (or (re-find (re-matcher re (:doc (meta example))))
+			 (or (re-find (re-matcher re (or (:doc (meta example)) "")))
                              (re-find (re-matcher re (:example (meta example))))))]
                (print-example example)))))
